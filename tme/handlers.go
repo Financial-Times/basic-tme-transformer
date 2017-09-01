6a4a4d09984bd449890c98245d3b783efafbcb2b
Euan Finlay <euan.finlay@ft.com>
2017-09-01 12:40:56 +0100

    fixing Sirupsen case sensitivity

--- tme/handlers.go
+++ tme/handlers.go
@@ -12,7 +12,7 @@ import (
 	"github.com/Financial-Times/go-fthealth/v1a"
 	"github.com/Financial-Times/service-status-go/gtg"
 	"github.com/Financial-Times/transactionid-utils-go"
-	log "github.com/Sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
